c3ddb752e132ed3415696414bc321b382ceb42e3
mac book pro <macbookpro@macs-MacBook-Pro.local>
2024-01-04 23:17:24 +0400

    start

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var dropdownOptions = document.querySelector(".dropdown-options");
+var dropdownButton = document.querySelector(".dropdown-button");
+var dropdownoption = document.querySelector(".dropdown-option");
+
+dropdownButton.addEventListener("click", () => {
+
+  dropdownOptions.style.display = dropdownOptions.style.display ==='none'?'block':'none';
+});
